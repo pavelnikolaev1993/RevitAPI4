7703651051711ec8ee906d2572dfa8a926663aa8
Павел Николаев <pavelnikolaev1993@mail.ru>
2022-04-20 19:11:19 +0300

    Добавьте файлы проекта.

---
+++ Main.cs
@@ -0,0 +1,45 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI4
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            string wallInfo = string.Empty;
+
+            var walls = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Walls)
+                .WhereElementIsNotElementType()
+                .Cast<Wall>()
+                .ToList();
+
+            foreach (Wall wall in walls)
+            {
+                string wallName = wall.get_Parameter(BuiltInParameter.SYMBOL_NAME_PARAM).AsString();
+                double wallVolume = wall.get_Parameter(BuiltInParameter.HOST_VOLUME_COMPUTED).AsDouble();
+                
+                wallInfo += $"{wallName} \t { wallVolume} {Environment.NewLine}";
+            }
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string csvPath = Path.Combine(desktopPath, "wallInfo.csv");
+            File.WriteAllText(csvPath, wallInfo);
+
+            return Result.Succeeded;
+        }
+    }
+}
